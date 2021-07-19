46943c91cfb9cfa418e78aedeb76f443977fdb90
Ivan Kochurkin <kvanttt@gmail.com>
2021-07-19 21:27:45 +0300

    Drop return types for members in full names renaming mode, extend tests

--- Tests/MessageDeobfuscation/Program.cs
+++ Tests/MessageDeobfuscation/Program.cs
@@ -2,6 +2,14 @@ using System;
 
 namespace MessageDeobfuscation {
 	class Class {
+		public string Method(string param1, int param2) => "method";
+
+		public string Field = "field";
+
+		public string Property => "property";
+
+		public event EventHandler<string> Event;
+
 		public class NestedClass {
 			internal string Method(string param) {
 				throw new Exception($"Exception");
